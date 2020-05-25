bfcfe7ffe372ad07baaa90096b5e584bfa84c6b9
rahul <rahulverma@utexas.edu>
2020-05-25 11:46:21 -0500

    function for adding linearly extrapolated boundary conditions. only 2d right now, but works for arbitrary stencil sizes.

--- tests/gridHandler.py
+++ tests/gridHandler.py
@@ -33,6 +33,46 @@ class Grid:
             xv, yv, zv = np.meshgrid(x,y,z)
             
             return xv, yv, zv
+
+'''
+Function for adding extrapolated boundary conditions.
+Currently 2D only. Must add functionality for 3D
+'''
+def ghostExtrapolate(g, data, stencil):
+    ny, nx = np.shape(data)
+    
+    # at lower x-end:
+    sign = np.sign(data[:,0])
+    abs_diff = np.abs(data[:,0]-data[:,1])
+    slope = sign * abs_diff
         
-g = Grid(2,-1,1,1/100)
-        
+    lower_bdry_x = np.array([data[:,0] + slope * (sten + 1) for sten in np.arange(stencil)])[::-1].transpose()
+    
+    # at upper x-end:
+    sign = np.sign(data[:, nx - 1])
+    abs_diff = np.abs(data[:, nx - 1]-data[:, nx - 2])
+    slope = sign * abs_diff
+    
+    upper_bdry_x = np.array([data[:,nx - 1] + slope * (sten + 1) for sten in np.arange(stencil)]).transpose()
+    
+    
+    data = np.concatenate((lower_bdry_x, data, upper_bdry_x),axis=1)
+    
+    # at lower y-end:
+    sign = np.sign(data[0])
+    abs_diff = np.abs(data[0]-data[1])
+    slope = sign * abs_diff
+    
+    lower_bdry_y = np.array([data[0] + slope * (sten + 1) for sten in np.arange(stencil)])[::-1]
+    
+    # at upper y-end:
+    sign = np.sign(data[ny - 1])
+    abs_diff = np.abs(data[ny - 1]-data[ny - 2])
+    slope = sign * abs_diff
+    
+    upper_bdry_y = np.array([data[ny - 1] + slope * (sten + 1) for sten in np.arange(stencil)])
+    
+    data = np.concatenate((lower_bdry_y, data, upper_bdry_y), axis=0)
+
+    return data
+
