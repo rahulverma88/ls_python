a97bfc47ee09b294003f8009775884bfe0e066ec
rahul <rahulverma@utexas.edu>
2020-05-30 18:50:44 -0500

    added initial version of odeCFL 1st order integrator - to be tested with velocity term function

---
+++ tests/odeCFL.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 18:21:59 2020
+
+@author: rahul
+"""
+
+def odeCFL1(data, tSpan, rhsFunc, grid, rhsData, options):
+    
+    t = tSpan[0]
+    
+    while t < tSpan[1]:
+        rhs, stepBound = rhsFunc(data, grid, rhsData)
+        deltaT = min(options.factorCFL * stepBound, tSpan[1] - t, options.maxStep)
+        data_new = data + deltaT * rhs
+        data = data_new
+    
+    return t, data
